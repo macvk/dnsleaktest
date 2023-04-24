19a68a859bf136414e2fb40bfb52ccf2597a58ae
vadim <tutumbul@gmail.com>
2023-04-24 22:11:53 +0200

    Add interface parameter

--- dnsleaktest.sh
+++ dnsleaktest.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#Usage: ./dnsleaktest.sh [-i interface_ip_address]
 
 RED='\033[0;31m'
 BOLD='\033[1m'
@@ -6,19 +7,32 @@ NC='\033[0m'
 api_domain='bash.ws'
 error_code=1
 
-function increment_error_code {
-    error_code=$((error_code + 1))
-}
+getopts "i:" opt
+interface=$OPTARG
 
 function echo_bold {
     echo -e "${BOLD}${1}${NC}"
+}
+
+if [ -z "$interface" ]; then
+    curl_interface=""
+    ping_interface=""
+else
+    curl_interface="--interface ${interface}"
+    ping_interface="-S${interface}"
+    echo_bold "Interface: ${interface}"
+    echo ""
+fi
+
+function increment_error_code {
+    error_code=$((error_code + 1))
 }
 
 function echo_error {
     (>&2 echo -e "${RED}${1}${NC}")
 }
 
-function program_exit {
+function require_command {
     command -v $1 > /dev/null
     if [ $? -ne 0 ]; then
         echo_error "Please, install \"$1\""
@@ -28,7 +42,7 @@ function program_exit {
 }
 
 function check_internet_connection {
-    curl --silent --head  --request GET "https://${api_domain}" | grep "200 OK" > /dev/null
+    curl --silent --head ${curl_interface} --request GET "https://${api_domain}" | grep "200 OK" > /dev/null
     if [ $? -ne 0 ]; then
         echo_error "No internet connection."
         exit $error_code
@@ -36,8 +50,8 @@ function check_internet_connection {
     increment_error_code
 }
 
-program_exit curl
-program_exit ping
+require_command curl
+require_command ping
 check_internet_connection
 
 if command -v jq &> /dev/null; then
@@ -53,14 +67,14 @@ else
 fi
 
 for i in $(seq 1 10); do
-    ping -c 1 "${i}.${id}.${api_domain}" > /dev/null 2>&1
+    ping -c 1 ${ping_interface} "${i}.${id}.${api_domain}" > /dev/null 2>&1
 done
 
 function print_servers {
 
-    if (( "$jq_exists" )); then
+    if (( $jq_exists )); then
 
-        echo "${result_json}" | \
+        echo ${result_json} | \
             jq  --monochrome-output \
             --raw-output \
             ".[] | select(.type == \"${1}\") | \"\(.ip)\(if .country_name != \"\" and  .country_name != false then \" [\(.country_name)\(if .asn != \"\" and .asn != false then \" \(.asn)\" else \"\" end)]\" else \"\" end)\""
@@ -72,10 +86,10 @@ function print_servers {
                 continue
             fi
 
-            ip=$(echo "$line" | cut -d'|' -f 1)
-            code=$(echo "$line" | cut -d'|' -f 2)
-            country=$(echo "$line" | cut -d'|' -f 3)
-            asn=$(echo "$line" | cut -d'|' -f 4)
+            ip=$(echo $line | cut -d'|' -f 1)
+            code=$(echo $line | cut -d'|' -f 2)
+            country=$(echo $line | cut -d'|' -f 3)
+            asn=$(echo $line | cut -d'|' -f 4)
 
             if [ -z "${ip// }" ]; then
                  continue
@@ -96,10 +110,10 @@ function print_servers {
 }
 
 
-if (( "$jq_exists" )); then
-    result_json=$(curl --silent "https://${api_domain}/dnsleak/test/${id}?json")
+if (( $jq_exists )); then
+    result_json=$(curl ${curl_interface} --silent "https://${api_domain}/dnsleak/test/${id}?json")
 else
-    result_txt=$(curl --silent "https://${api_domain}/dnsleak/test/${id}?txt")
+    result_txt=$(curl ${curl_interface} --silent "https://${api_domain}/dnsleak/test/${id}?txt")
 fi
 
 dns_count=$(print_servers "dns" | wc -l)
@@ -108,10 +122,10 @@ echo_bold "Your IP:"
 print_servers "ip"
 
 echo ""
-if [ "${dns_count}" -eq "0" ];then
+if [ ${dns_count} -eq "0" ];then
     echo_bold "No DNS servers found"
 else
-    if [ "${dns_count}" -eq "1" ];then
+    if [ ${dns_count} -eq "1" ];then
         echo_bold "You use ${dns_count} DNS server:"
     else
         echo_bold "You use ${dns_count} DNS servers:"
